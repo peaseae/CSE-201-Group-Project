7de9880830f7d6904e1b80c1b42e6dc59a37bdd0
CaptainHCao <92411254+CaptainHCao@users.noreply.github.com>
2021-10-19 23:35:12 -0400

    Update SearchBar.java

--- SearchBar.java
+++ SearchBar.java
@@ -31,3 +31,36 @@ public class SearchBar {
 		System.out.println(Arrays.toString(search(data)));
 	}
 }
+
+/*
+import java.util.*;
+
+public class SearchBar {
+	private static String[] data = {"apple", "boat", "candy", "delta", "epsilon", "either", "dipper"};
+	private static int size = data.length;
+
+
+	public static ArrayList<String> search(String[] data) {
+		ArrayList<String> output = new ArrayList<String>();
+		Scanner input = new Scanner(System.in);
+		System.out.println("Search: ");
+		String find = input.nextLine(); 
+			for(int i = 0; i < size; i++) {
+				if (find.equals(data[i])) {
+					output.add(data[i]);
+				}
+			}
+			input.close();
+			if (output.size() == 0) {	
+				output.add("There is no such application.");
+			return output;
+			} else {
+				return output;
+			}
+		}
+
+	public static void main(String[] args) {
+		System.out.println(search(data));
+	}
+}
+*/
