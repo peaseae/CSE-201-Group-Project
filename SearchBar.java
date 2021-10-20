bc04dfeb450d3bdf90de844d742c1250f62970ac
egbertkd <92411241+egbertkd@users.noreply.github.com>
2021-10-19 23:02:59 -0400

    Add files via upload

---
+++ SearchBar.java
@@ -0,0 +1,33 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class SearchBar {
+	private static String[] data = {"apple", "boat", "candy", "delta", "epsilon", "either", "dipper"};
+	private static int size = data.length;
+
+
+	public static String[] search(String[] data) {
+		String[] output = new String[1];
+		Scanner input = new Scanner(System.in);
+		System.out.println("Search: ");
+		String find = input.nextLine();
+			for(int i = 0; i < size; i++) {
+				int place = 0;
+				if (find.equals(data[i])) {
+					output[place] = data[i];
+					place++;
+				}
+			}
+		if (output[0] == null) {	
+			output[0] = "There is no such application.";
+			return output;
+		} else {
+			return output;
+		}
+	}
+
+	public static void main(String[] args) {
+		System.out.println(Arrays.toString(search(data)));
+	}
+}
