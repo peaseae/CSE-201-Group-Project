3121335b8024d424e33caff3eff043f8f392cb5d
peaseae <92066509+peaseae@users.noreply.github.com>
2021-11-14 20:51:08 -0500

    Add files via upload

--- Demo1.java
+++ Demo1.java
@@ -1,21 +1,17 @@
-import java.awt.EventQueue;
-
 import javax.swing.JFrame;
-import java.awt.Color;
-import java.awt.Component;
-
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
-
-import java.awt.Font;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
-import javax.swing.text.DefaultCaret;
 
+import java.awt.EventQueue;
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.event.ActionListener;
+
 import java.awt.event.ActionEvent;
 import java.util.ArrayList;
 import java.util.Date;
@@ -65,34 +61,37 @@ public class Demo1 {
      * Initialize the contents of the frame.
      */
     private void initialize() {
+        // create frame
         frame = new JFrame();
         frame.getContentPane().setBackground(new Color(210, 225, 225));
         frame.getContentPane().setLayout(null);
+        frame.setBounds(100, 100, 800, 600);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         
+        // create log in and sign up buttons
         JButton loginBtn = new JButton("Login");
         loginBtn.setBounds(558, 21, 100, 25);
         frame.getContentPane().add(loginBtn);
-        
         JButton signUpBtn = new JButton("Sign up");
         signUpBtn.setBounds(670, 21, 100, 25);
         frame.getContentPane().add(signUpBtn);
         
+        // create title
         JLabel applicationsLabel = new JLabel("Browse App");
-        applicationsLabel.setFont(new Font("Segoe UI", Font.PLAIN, 17));
-        applicationsLabel.setBounds(40, 13, 189, 36);
+        applicationsLabel.setFont(new Font("Segoe UI", Font.PLAIN, 30));
+        applicationsLabel.setBounds(40, 13, 189, 50);
         frame.getContentPane().add(applicationsLabel);
         
+        // create scrolling result panel
         JPanel resultPanel = new JPanel();
         resultPanel.setBackground(Color.white);
         resultPanel.setLayout(new BoxLayout(resultPanel, BoxLayout.Y_AXIS));
-        
         JScrollPane scroll = new JScrollPane(resultPanel);
         scroll.setBackground(Color.white);
         scroll.setBounds(40, 195, 532, 302);
         frame.getContentPane().add(scroll);
-        frame.setBounds(100, 100, 800, 600);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         
+        // create platform filter
         JComboBox platformBox = new JComboBox();
         String[] platforms = {"Select Platform", "iOS", "Android", "Window", "Linux"};
         for (String platform: platforms) {
@@ -101,6 +100,7 @@ public class Demo1 {
         platformBox.setBounds(614, 195, 124, 42);
         frame.getContentPane().add(platformBox);
         
+        // create price filter
         JComboBox priceBox = new JComboBox();
         String[] prices = {"Select Price", "Free", "$0.00 - $0.99", "$1.00 - $4.99", "$5.00 or more"};
         for (String price: prices) {
@@ -109,6 +109,7 @@ public class Demo1 {
         priceBox.setBounds(614, 261, 124, 42);
         frame.getContentPane().add(priceBox);
         
+        // create sort option
         JComboBox sortBox = new JComboBox();
         String[] sorts = {"Sort by...", "App Name A - Z", "App Name Z - A", "Date Added (Newest)", "Date Added (Oldest)"};
         for (String sort: sorts) {
@@ -117,96 +118,53 @@ public class Demo1 {
         sortBox.setBounds(614, 327, 124, 42);
         frame.getContentPane().add(sortBox);
         
+        // create search button and search field
         JButton searchBtn = new JButton("Search");
         searchBtn.setBounds(614, 129, 124, 42);
         frame.getContentPane().add(searchBtn);
-        
-      searchBtn.addActionListener(new ActionListener() {
-          public void actionPerformed(ActionEvent arg0) {
-              // clear panel of any old search results
-              resultPanel.removeAll();
-              resultPanel.revalidate();
-              resultPanel.repaint();
-              // take in search input
-              String key = searchField.getText();
-              // take in platform, price range, and sorting method from combo boxes
-              String platformKey = (String)(platformBox.getSelectedItem());
-              String priceRangeKey = (String)(priceBox.getSelectedItem());
-              String sortKey = (String)(sortBox.getSelectedItem());
-              // create output using the search word, filters, and sorting method
-              output = SearchBar.search(apps, key, priceRangeKey, platformKey, sortKey);
-              // display
-              try {
-                  if (output.size() == 0) {
-                      JTextArea textMessage = new JTextArea();
-                      textMessage.setText("\n  No results found.\n  Maybe try searching something else?");
-                      resultPanel.add(textMessage);
-                  }
-                  for (Application app : output) {
-                      JButton temp = new JButton(app.displayHtml());
-                      temp.setHorizontalAlignment(SwingConstants.LEFT);
-                      temp.setBackground(Color.WHITE);
-                      resultPanel.add(temp);
-                  }
-              }
-              catch (Exception e) {
-                  JTextArea textMessage = new JTextArea();
-                  textMessage.setText("No results found.");
-                  resultPanel.add(textMessage);
-              }
-          }
-      });
-        
-//        searchBtn.addActionListener(new ActionListener() {
-//            public void actionPerformed(ActionEvent arg0) {
-//                textArea.setText("");
-//                String key = searchField.getText();
-//                // take in platform, price range, and sorting method from combo boxes
-//                String platformKey = (String)(platformBox.getSelectedItem());
-//                String priceRangeKey = (String)(priceBox.getSelectedItem());
-//                String sortKey = (String)(sortBox.getSelectedItem());
-//                // create output using the search word, filters, and sorting method
-//                output = SearchBar.search(apps, key, priceRangeKey, platformKey, sortKey);
-//                // display
-//                try {
-//                    if (output.size() == 0) {
-//                        textArea.setText("No results found.");
-//                    }
-//                    for (Application app : output) {
-//                        textArea.append(app.display() + "\n");
-//                        JButton appButton = new JButton(app.getName());
-//                        scroll.add(appButton);
-//                    }
-//                }
-//                catch (Exception e) {
-//                    textArea.setText("There was an error displaying the output.");
-//                }
-//            }
-//        });
-        
-        
-        
         searchField = new JTextField();
-//        searchField.addActionListener(new ActionListener() {
-//            public void actionPerformed(ActionEvent arg0) {
-//                textArea.setText("");
-//                String key = searchField.getText();
-//                output = SearchBar.search(apps, key);
-//                try {
-//                	if (output.size() == 0) {
-//                        textArea.setText("No results found.");
-//                    }
-//                    for (Application app : output) {
-//                        textArea.append(app.display() + "\n");
-//                    }
-//                }
-//                catch (Exception e) {
-//                    textArea.setText("No results founds.");
-//                }
-//            }
-//        });
         searchField.setBounds(40, 129, 532, 42);
         frame.getContentPane().add(searchField);
         searchField.setColumns(10);
+        
+        // create action listener to display search results in result panel
+        ActionListener searchAction = new ActionListener() {
+            public void actionPerformed(ActionEvent arg0) {
+                // clear panel of any old search results
+                resultPanel.removeAll();
+                resultPanel.revalidate();
+                resultPanel.repaint();
+                // take in search input
+                String key = searchField.getText();
+                // take in platform, price range, and sorting method from combo boxes
+                String platformKey = (String)(platformBox.getSelectedItem());
+                String priceRangeKey = (String)(priceBox.getSelectedItem());
+                String sortKey = (String)(sortBox.getSelectedItem());
+                // create output using the search word, filters, and sorting method
+                output = SearchBar.search(apps, key, priceRangeKey, platformKey, sortKey);
+                // display
+                try {
+                    if (output.size() == 0) {
+                        JTextArea textMessage = new JTextArea();
+                        textMessage.setText("\n  No results found.\n  Maybe try searching something else?");
+                        resultPanel.add(textMessage);
+                    }
+                    for (Application app : output) {
+                        JButton temp = new JButton(app.displayHtml());
+                        temp.setHorizontalAlignment(SwingConstants.LEFT);
+                        temp.setBackground(Color.WHITE);
+                        resultPanel.add(temp);
+                    }
+                }
+                catch (Exception e) {
+                    JTextArea textMessage = new JTextArea();
+                    textMessage.setText("No results found.");
+                    resultPanel.add(textMessage);
+                }
+            }
+        };
+        // do search action when search button is clicked or enter is pressed
+        searchBtn.addActionListener(searchAction);
+        searchField.addActionListener(searchAction);
     }
 }
